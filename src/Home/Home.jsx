71b16bfa34d7fdb11c8aca20c09d61ca5d752338
Suprio-Das <priyamd507@gmail.com>
2025-03-02 23:45:00 +0600

    React Icons installed and set to the home banner

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,10 @@
 import './Home.css'
+import { GiNotebook } from "react-icons/gi";
 
 const Home = () => {
     return (
         <div className="mx-auto home-banner p-11">
-            <h1 className='text-4xl text-center text-white font-semibold'>Your To-Do</h1>
+            <h1 className='text-4xl text-center text-white font-semibold flex justify-center gap-3'><GiNotebook /> Your To-Do</h1>
             <p className='text-2xl text-center text-white'>Don't Forget Anymore, Save Here!</p>
         </div>
     );
