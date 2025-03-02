d01b5bfbfdc76cf53ccf81d7d880fdd66f0c866d
Suprio-Das <priyamd507@gmail.com>
2025-03-03 00:09:55 +0600

    Form adjusted

--- src/ToDoForm/ToDoForm.jsx
+++ src/ToDoForm/ToDoForm.jsx
@@ -1,12 +1,18 @@
 const ToDoForm = () => {
     return (
         <div>
-            <h1 className="text-center text-xl font-semibold">List Your Work</h1>
-            <div className="w-3/5 mx-auto">
-                <form className="flex flex-col items-center gap-2">
-                    <input type="text" placeholder="Meeting at 3:00 PM" className="input" />
-                    <input type="textarea" placeholder="Meeting at 3:00 PM" className="textarea" />
-                    <button className="btn">Add To List</button>
+            <h1 className="text-center text-xl font-semibold my-2">List Your Work</h1>
+            <div className="mx-auto flex justify-center items-center w-[30%]">
+                <form className="w-full">
+                    <div>
+                        <input type="text" placeholder="Meeting at 3:00 PM" className="input w-full mt-3" />
+                    </div>
+                    <div>
+                        <input type="textarea" placeholder="Description of the List..." className="textarea w-full mt-3" />
+                    </div>
+                    <div>
+                        <input type="submit" value="Add to List" className="btn w-full mt-3" />
+                    </div>
                 </form>
             </div>
         </div>
