b02c4431e3e553ee115bb53255a9a7ebb6bff4e7
Suprio-Das <priyamd507@gmail.com>
2025-03-02 23:40:04 +0600

    Home Banner Added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,10 @@
+import './Home.css'
+
 const Home = () => {
     return (
-        <div>
-            <h1>This is home.</h1>
+        <div className="mx-auto home-banner p-11">
+            <h1 className='text-4xl text-center text-white font-semibold'>Your To-Do</h1>
+            <p className='text-2xl text-center text-white'>Don't Forget Anymore, Save Here!</p>
         </div>
     );
 };
