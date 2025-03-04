d922763aa8b6fe2cd8c694a9916e17661907b5f5
Suprio-Das <priyamd507@gmail.com>
2025-03-04 14:35:53 +0600

    Dynamic modal added to the Edit button

--- src/ToDoCard/ToDoCard.jsx
+++ src/ToDoCard/ToDoCard.jsx
@@ -4,7 +4,6 @@ import { MdDelete } from "react-icons/md";
 import { Link } from 'react-router';
 
 const ToDoCard = ({ singleToDo }) => {
-    console.log(singleToDo)
     return (
         <div className='p-5 border-2 border-base-300 rounded-lg max-h-52 flex flex-col'>
             <div className='flex-grow'>
@@ -12,9 +11,22 @@ const ToDoCard = ({ singleToDo }) => {
                 <p className='text-sm text-justify'>{singleToDo.description}</p>
             </div>
             <div className='mt-auto'>
-                <Link className='btn bg-amber-400 mr-2'>Edit <MdModeEdit /> </Link>
+                <Link onClick={() => document.getElementById(`${singleToDo._id}`).showModal()} className='btn bg-amber-400 mr-2'>Edit <MdModeEdit /> </Link>
                 <Link className='btn bg-red-500 mr-2 text-white'>Delete <MdDelete /> </Link>
             </div>
+
+            {/* Update Modal */}
+            <dialog id={`${singleToDo._id}`} className="modal">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Hello from {singleToDo.name}!</h3>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
         </div>
 
     );
